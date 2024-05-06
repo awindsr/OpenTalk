355cc244ae29694ee1fdd8de0ae840a9bf55060f
Alan Joy <alanjoy456@gmail.com>
2024-05-06 10:19:55 +0530

    hii

--- Routes/script.js
+++ Routes/script.js
@@ -2,10 +2,19 @@ import express from "express";
 const router = express.Router();
 import user from "../models/user.js";
 import passport from "passport";
+import localStrategy from "passport-local";
+import expressSession from "express-session";
 
 
 router.use(express.urlencoded({ extended: true }));
 
+
+passport.use(new localStrategy(user.authenticate()));
+router.use(expressSession({
+    resave:false,
+    saveUninitialized:false,
+    secret :"hey"
+  }))
 
 router.get("/", ( req, res)=>{
     res.render("start.ejs");
@@ -18,7 +27,7 @@ router.get("/registerPage", (req,res)=>{
 router.post("/registerPage",(req,res,next)=>{
     const userData = new user({
         username: req.body.username,
-        password: req.body.password,
+        password:req.body.password,
         fullname: req.body.fullname
     });
     console.log(req.body);
@@ -35,6 +44,12 @@ router.get("/loginPage",(req,res)=>{
     res.render("login.ejs");
 });
 
+router.post("/loginPage",passport.authenticate("local",{
+    successRedirect :"/home",
+    failureRedirect : "/loginPage",
+    // failureFlash: true
+  }),function(req,res){
+  })
 // function isLoggedIn(req,res,next){
 //     if(req.isAuthenticated()) return next();
 //     res.redirect("/");
--- app.js
+++ app.js
@@ -2,9 +2,6 @@ import express from "express";
 import router from './Routes/script.js';
 import user from "./models/user.js";
 import passport from "passport";
-import localStrategy from "passport-local";
-import expressSession from "express-session";
-
 
 const app=express();
 const PORT=3000;
@@ -14,15 +11,10 @@ app.set("view engine", "ejs");
 app.use('/', router);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json())
 
 ////////////////////////////////////////////////////////////
 
-passport.use(new localStrategy(user.authenticate()));
-app.use(expressSession({
-    resave:false,
-    saveUninitialized:false,
-    secret :"hey"
-  }))
 app.use(passport.initialize());
 app.use(passport.session());
 passport.serializeUser(user.serializeUser());
