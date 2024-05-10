6c2056d9a6d6d2d570eb0df04cee3994cd71f959
AmalJoy14 <amaljoy246@gmail.com>
2024-05-10 19:32:30 +0530

    updated

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ import flash from "connect-flash";
 import { Server } from "socket.io";
 import { createServer } from 'node:http';
 
-const app=express();
+const app = express();
 const server = createServer(app);
-const io = new Server(server,{
+const io = new Server(server, {
     connectionStateRecovery: {}
 });
-const PORT=3000;
+const PORT = 3000;
 
 app.set("view engine", "ejs");
 
@@ -24,10 +24,10 @@ app.use(flash());
 ////////////////////////////////////////////////////////////
 passport.use(new localStrategy(user.authenticate()));
 app.use(expressSession({
-    resave:false,
-    saveUninitialized:false,
-    secret :"hey"
-  }))
+    resave: false,
+    saveUninitialized: false,
+    secret: "hey"
+}))
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -35,15 +35,15 @@ passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
 
 ////////////////////////////////////////////////////////////
-app.get("/", ( req, res)=>{
+app.get("/", (req, res) => {
     res.render("start.ejs");
 });
 
-app.get("/register", (req,res)=>{
-    res.render("register.ejs",{existsError : req.flash('error')});
+app.get("/register", (req, res) => {
+    res.render("register.ejs", { existsError: req.flash('error') });
 });
 
-app.post("/register",(req,res,next)=>{
+app.post("/register", (req, res, next) => {
     const userData = new user({
         username: req.body.username,
         password: req.body.password,
@@ -54,53 +54,60 @@ app.post("/register",(req,res,next)=>{
         if (err) {
             // If the error is due to a duplicate username
             if (err.name === 'UserExistsError') {
-                req.flash("error","Username already exists.");
+                req.flash("error", "Username already exists.");
                 return res.redirect('/register');
             }
         }
         // Registration successful
-        passport.authenticate("local")(req,res,()=>{
-            res.redirect("/home");
+        passport.authenticate("local")(req, res, () => {
+            res.redirect("/home/username");
+        })
     })
-   })
 })
 
 
-app.get("/login",(req,res)=>{
-    res.render("login.ejs",{ error:req.flash("error")});
+app.get("/login", (req, res) => {
+    res.render("login.ejs", { error: req.flash("error") });
 });
 
-app.post("/login",passport.authenticate("local",{
-    successRedirect :"/home",
-    failureRedirect : "/login",
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
     failureFlash: true,
-  }),function(req,res){
-  })
+}), function (req, res) {
+})
 
-app.get("/logout",(req,res,next)=>{
-    req.logout((err)=> {
-      if (err) { return next(err); }
-      res.redirect('/login');
+app.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err); }
+        res.redirect('/login');
     });
 });
 
-app.get("/home",isLoggedIn,(req,res)=>{
+app.get("/home", isLoggedIn, (req, res) => {
+    const username = req.user.username;
+    res.redirect("/home/" + username);
+});
+
+app.get("/home/:username", isLoggedIn, (req, res) => {
+    const username = req.params.username;
+    
     res.render("home.ejs");
 });
 
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()) return next();
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
     res.redirect("/login");
-  }
+}
 
 io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('chat message', (msg) => {
-        io.emit('chat message',msg);
-      });
+        io.emit('chat message', msg);
+    });
     socket.on('disconnect', () => {
         console.log('A user disconnected');
-      });
+    });
 });
 
 server.listen(PORT, () => {
