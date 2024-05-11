d124d8300f59414aa5b6f9a9d28cf1464542f57d
Alan Joy <alanjoy456@gmail.com>
2024-05-11 22:14:20 +0530

    modified

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import localStrategy from "passport-local";
 import expressSession from "express-session";
 import flash from "connect-flash";
 import { Server } from "socket.io";
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    connectionStateRecovery: {}
+  connectionStateRecovery: {},
 });
 const PORT = 3000;
 
@@ -18,16 +18,18 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 app.use(flash());
 
 ////////////////////////////////////////////////////////////
 passport.use(new localStrategy(user.authenticate()));
-app.use(expressSession({
+app.use(
+  expressSession({
     resave: false,
     saveUninitialized: false,
-    secret: "hey"
-}))
+    secret: "hey",
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -36,90 +38,104 @@ passport.deserializeUser(user.deserializeUser());
 
 ////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
-    res.render("start.ejs");
+  res.render("start.ejs");
 });
 
 app.get("/register", (req, res) => {
-    res.render("register.ejs", { existsError: req.flash('error') });
+  res.render("register.ejs", { existsError: req.flash("error") });
 });
 
 app.post("/register", (req, res, next) => {
-    const userData = new user({
-        username: req.body.username,
-        password: req.body.password,
-        fullname: req.body.fullname
+  const userData = new user({
+    username: req.body.username,
+    password: req.body.password,
+    fullname: req.body.fullname,
+  });
+  console.log(req.body);
+  user.register(userData, req.body.password, (err) => {
+    if (err) {
+      // If the error is due to a duplicate username
+      if (err.name === "UserExistsError") {
+        req.flash("error", "Username already exists.");
+        return res.redirect("/register");
+      }
+    }
+    // Registration successful
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/home");
     });
-    console.log(req.body);
-    user.register(userData, req.body.password, (err) => {
-        if (err) {
-            // If the error is due to a duplicate username
-            if (err.name === 'UserExistsError') {
-                req.flash("error", "Username already exists.");
-                return res.redirect('/register');
-            }
-        }
-        // Registration successful
-        passport.authenticate("local")(req, res, () => {
-            res.redirect("/home");
-        })
-    })
-})
-
-
-app.get("/login", (req, res) => {
-    res.render("login.ejs", { error: req.flash("error") });
+  });
 });
 
-app.post("/login", passport.authenticate("local", {
+app.get("/login", (req, res) => {
+  res.render("login.ejs", { error: req.flash("error") });
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/home",
     failureRedirect: "/login",
     failureFlash: true,
-}), function (req, res) {
-})
+  }),
+  function (req, res) {}
+);
 
 app.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if (err) { return next(err); }
-        res.redirect('/login');
-    });
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
 });
 
 app.get("/home", isLoggedIn, (req, res) => {
-    const username = req.session.passport.user;
-    res.redirect("/home/" + username);
+  const username = req.session.passport.user;
+  res.redirect("/home/" + username);
 });
 
 app.get("/home/:username", isLoggedIn, (req, res) => {
-    if(req.params.username !== req.session.passport.user){
-        res.redirect("/login");
-    }
-    else{
-        res.render("home.ejs");
-    }
+  if (req.params.username !== req.session.passport.user) {
+    res.redirect("/login");
+  } else {
+    res.render("home.ejs");
+  }
 });
 
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect("/login");
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
 }
 //socket io code//////////////////////////////////////////
 
-io.on('connection', (socket) => {
-    console.log('a user connected :'+ socket.id);
-    socket.on('disconnect', () => {
-        console.log('user disconnected :'+ socket.id);
-      });
+io.on("connection", (socket) => {
+  console.log("a user connected :" + socket.id);
+  socket.on("disconnect", () => {
+    socket.leave("some room");
+    console.log("user disconnected :" + socket.id);
   });
+});
+//             Global Chat
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//       socket.broadcast.emit('chat message', msg);
+//     });
+//   });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      socket.broadcast.emit('chat message', msg);
-    });
+io.on("connection", (socket) => {
+    socket.join("some room");
+
+    socket.on("chat message", (msg) => {
+    // Broadcast to all connected clients in the room except the sender
+    socket.to("some room").emit("chat message", msg);
+
   });
+  // Join the room named 'some room'
+});
 
 //////////////////////////////////////////////////////////
 
 server.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}`);
+  console.log(`Listening to port ${PORT}`);
 });
-
