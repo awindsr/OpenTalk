9ee5d341ba2235e8c7c2933e25da8ed9897cf41d
AmalJoy14 <amaljoy246@gmail.com>
2024-05-14 14:34:36 +0530

    updated

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -5,3 +5,18 @@ if (friendList.innerText===''){
     friendList.innerHTML="<img class='noUserFoundImage' src='/images/noUsersFound.png'>";
     friendList.classList.add("center");
 }
+
+
+//Selecting a friend
+const allFriendsList = document.querySelectorAll(".user1");
+console.log(allFriendsList);
+
+for (let friend of allFriendsList){
+    friend.addEventListener("click",(e)=>{
+        console.log("clicked");
+        friend.classList.add("clicked");
+        setTimeout(()=>{
+            friend.classList.remove("clicked");
+        },300)
+    })
+}
