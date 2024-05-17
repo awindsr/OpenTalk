1ac7a96e55c5f1d812129fed9d4dd78c83f59503
Alan Joy <alanjoy456@gmail.com>
2024-05-17 15:18:32 +0530

    changes made

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,54 +1,97 @@
 const socket = io({
-    auth: {
-      serverOffset: 0
-    }
-  });
-
-
-/*************************************************************Global Chat***************************************************************/
-const form = document.getElementById('globalForm');
-const username = form.getAttribute("username");
-const input = document.getElementById('globalInput');
-const messages = document.getElementById('globalMessagediv');
-
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (input.value) {
-        ////////////////////////////////////////////
-        const item = document.createElement('div');
-        item.classList.add("mymessage");
-        item.innerHTML ="<span>"+ input.value +"(You)</span>"
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-        ////////////////////////////////////////////
-        socket.emit('Global Chat', input.value,username);
-        input.value = '';
-    }
+  auth: {
+    serverOffset: 0,
+  },
 });
 
-socket.on('Global Chat', (msg , username ) => {
-    const item = document.createElement('div');
-    item.classList.add("othermessage");
-    item.innerHTML ="<span>"+msg+ "("+username+")</span>"
+/*************************************************************Global Chat***************************************************************/
+const form = document.getElementById("globalForm");
+const username = form.getAttribute("username");
+const input = document.getElementById("globalInput");
+const messages = document.getElementById("globalMessagediv");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value) {
+    ////////////////////////////////////////////
+    const item = document.createElement("div");
+    item.classList.add("mymessage");
+
+    const innerItem = document.createElement("span");
+
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("mynametag");
+    innerDiv.innerText = "You";
+
+    innerItem.appendChild(innerDiv);
+
+    // Append the user input value to the span, after the name tag div
+    const messageText = document.createTextNode(input.value);
+    innerItem.appendChild(messageText);
+
+    // Append the span to the main container div
+    item.appendChild(innerItem);
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
-  });
+    ////////////////////////////////////////////
+    socket.emit("Global Chat", input.value, username);
+    input.value = "";
+  }
+});
+
+socket.on("Global Chat", (msg, username) => {
+
+  const item = document.createElement("div");
+  item.classList.add("othermessage");
+
+  const innerItem = document.createElement("span");
+
+  const innerDiv = document.createElement("div");
+  innerDiv.classList.add("othernametag");
+  innerDiv.innerText = username;
+
+  innerItem.appendChild(innerDiv);
+
+  // Append the user input value to the span, after the name tag div
+  const messageText = document.createTextNode(msg);
+  innerItem.appendChild(messageText);
+
+  // Append the span to the main container div
+  item.appendChild(innerItem);
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
 
 // Handle recovered messages
-socket.on('Recover messages', (msg,username) => {
-    const item = document.createElement('div');
-    if (form.getAttribute("username")===username){
-        item.classList.add("mymessage");
-        item.innerHTML = "<span>" + msg + " (You)</span>";
-    }
-    else{
-        item.classList.add("othermessage");
-        item.innerHTML = "<span>" + msg + " (" + username + ")</span>";
-    }
-    
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-  });
+socket.on("Recover messages", (msg, username) => {
+  const item = document.createElement("div");
+  if (form.getAttribute("username") === username) {
 
-  
+    item.classList.add("mymessage");
+    const innerItem = document.createElement("span");
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("mynametag");
+    innerDiv.innerText = "You";
+    innerItem.appendChild(innerDiv);
+    const messageText = document.createTextNode(msg);
+    innerItem.appendChild(messageText);
+    item.appendChild(innerItem);
+
+  } else {
+
+    item.classList.add("othermessage");
+    const innerItem = document.createElement("span");
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("othernametag");
+    innerDiv.innerText = username;
+    innerItem.appendChild(innerDiv);
+    const messageText = document.createTextNode(msg);
+    innerItem.appendChild(messageText);
+    item.appendChild(innerItem);
+    
+  }
+
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
